dcf5a890206999c3040298dab91419fe57103a4f
Matthew Hyatt <mhyatt000@gmail.com>
2025-12-07 20:18:57 -0600

    new main entrypoint

--- plugins/server_wilor/main.py
+++ plugins/server_wilor/main.py
@@ -1,4 +1,19 @@
-from .src.server_wilor.server import WilorModel
+from server_wilor.wilor import WilorModel
+import tyro
+from dataclasses import dataclass
 
-def load():
-    return WilorModel()
+@dataclass
+class WilorConfig:
+    pass # TODO
+
+def main(): # cfg: WilorConfig
+    # TODO
+
+    policy = Wilor()
+    # TODO
+    # server = ...
+    server.serve()
+
+if __name__ == "__main__":
+    # TODO
+    # main(tyro.cli(WilorConfig))
