0c81283f4682de7cb099223105970ee08af156ef
Matthew Hyatt <mhyatt000@gmail.com>
2025-12-09 18:05:34 -0600

    ruff and precommit

--- plugins/server_wilor/main.py
+++ plugins/server_wilor/main.py
@@ -1,23 +1,24 @@
-import sys
-from pathlib import Path
 from dataclasses import dataclass
-import tyro
-import torch
-import cv2
 
-from wilor_mini.pipelines.wilor_hand_pose3d_estimation_pipeline import WiLorHandPose3dEstimationPipeline
+import cv2
+import torch
+import tyro
 from webpolicy.base_policy import BasePolicy
 from webpolicy.server import Server
+from wilor_mini.pipelines.wilor_hand_pose3d_estimation_pipeline import (
+    WiLorHandPose3dEstimationPipeline,
+)
 
 # from server_wilor.server import WilorModel
+
 
 @dataclass
 class WilorConfig:
     host: str = "0.0.0.0"
     port: int = 8007
 
-def demo():
 
+def demo():
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     dtype = torch.float16
 
@@ -29,11 +30,10 @@ def demo():
     print(outputs)
 
     print(len(outputs))
-    print(outputs[0]['wilor_preds'].keys())
+    print(outputs[0]["wilor_preds"].keys())
 
-    
+
 class WilorPolicy(BasePolicy):
-
     def __init__(self, cfg: WilorConfig):
         self.cfg = cfg
         self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -41,10 +41,11 @@ class WilorPolicy(BasePolicy):
         self.pipe = WiLorHandPose3dEstimationPipeline(device=self.device, dtype=dtype)
 
     def step(self, payload: dict) -> dict:
-        image = payload['image']
+        image = payload["image"]
         out = self.pipe.predict(image)
         # TODO use jax.tree.map(lambda *xs: np.stack([xs]) , *out) if multiple batch
         return out[0]
+
 
 def main(cfg: WilorConfig):
     """Standalone debug mode."""
