649e715a74cb41c47b26a8a70e0f72fa9ee2dcb3
Matthew Hyatt <mhyatt000@gmail.com>
2026-03-30 17:46:30 -0500

    add main entry from wilor mini

--- plugins/server_wilor/main.py
+++ plugins/server_wilor/main.py
@@ -1,5 +1,19 @@
+import torch
+import cv2
+from wilor_mini.pipelines.wilor_hand_pose3d_estimation_pipeline import WiLorHandPose3dEstimationPipeline
+
 def main():
     print("Hello from server-wilor!")
+
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    dtype = torch.float16
+
+    pipe = WiLorHandPose3dEstimationPipeline(device=device, dtype=dtype)
+    img_path = "assets/img.png"
+    image = cv2.imread(img_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    outputs = pipe.predict(image)
+
 
 
 if __name__ == "__main__":
